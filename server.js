5f822b13728fe5bb6b138508b0b061fa78d84a41
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-02-17 21:54:28 -0800

    added log for timestamps requests

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const http = require('http');
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
+  var addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
 
-console.log('My first Node test server is running on Port 8080.');
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
+    if (err) {
+      console.log(err);
+    }
+      else {
+    console.log('Added to log.');
+    }
+  });
+
+}).listen(8080);
